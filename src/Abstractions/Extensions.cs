f470951f912061a2c539c4ca8e8fb23730ad33c3
Thismaker <smithkare47@gmail.com>
2023-04-03 15:12:31 +0300

    add: tests and ToKMB method

---
+++ src/Abstractions/Extensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ocluse.LiquidSnow.Venus
+{
+    public static class Extensions
+    {
+        public static string ToKMB<T>(this T num, CultureInfo? cultureInfo = null) where T : INumber<T>
+        {
+            T trillion = T.Parse("999999999999", cultureInfo);
+            T billion = T.Parse("999999999", cultureInfo);
+            T million = T.Parse("999999", cultureInfo);
+            T thousand = T.Parse("999", cultureInfo);
+
+            if (num > trillion || num < -trillion)
+            {
+                return num.ToString("0,,,,.###T", cultureInfo);
+            }
+
+            else if (num > billion || num < -billion)
+            {
+                return num.ToString("0,,,.###B", cultureInfo);
+            }
+            else
+            if (num > million || num < -million)
+            {
+                return num.ToString("0,,.##M", cultureInfo);
+            }
+            else
+            if (num > thousand || num < -thousand)
+            {
+                return num.ToString("0,.#K", cultureInfo);
+            }
+            else
+            {
+                return num.ToString(format: null, formatProvider: cultureInfo);
+            }
+        }
+    }
+}
---
+++ tests/Ocluse.LiquidSnow.Venus.Abstractions.Tests/ExtensionsTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ocluse.LiquidSnow.Venus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ocluse.LiquidSnow.Venus.Tests
+{
+    [TestClass()]
+    public class ExtensionsTests
+    {
+        [TestMethod()]
+        public void ToKMBTest()
+        {
+            decimal num = 7890;
+
+            var result = num.ToKMB();
+
+            Assert.AreEqual("7.9K", result);
+        }
+    }
+}
